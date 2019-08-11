95b536f41c001b2fc3008320a1aecd8aa9815d83
canokaue <kauecano@gmail.com>
2019-08-11 15:29:47 -0300

    update flask

--- admin/zengrid/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZengridConfig(AppConfig):
-    name = 'zengrid'
--- admin/zengrid/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'zengrid/index.html')
