3d0bbd53bbc8ccac44b75d5d66831a8446e83dbf
canokaue <kauecano@gmail.com>
2019-08-11 18:06:25 -0300

    pitch time

---
+++ app/static/js/eth.js
@@ -0,0 +1,5 @@
+function butt() {
+    document.getElementById("add").innerHTML = "<p><span>Address:</span> 0x134dC212f53e265402b38713478fb0857cce5991</p>";
+    document.getElementById("hash").innerHTML = " <p><span>Hash: 0x2ec2356a5dc69e3bf61c52d9c58a42fdd85995d7</span> </p>";
+    document.getElementById("amm").innerHTML = "<p><span>Ammount: 2.43 ZNG <br> Rate:</span> 1.0</p>"; 
+}
